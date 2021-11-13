f9e003096ef0ce86c3854a8a4157596f8fa1c4e5
Santiago Mozzo <santiago.1997.mozzo@hotmail.com>
2021-11-13 18:03:48 -0300

    entrega 7

--- js/cart.js
+++ js/cart.js
@@ -4,7 +4,6 @@
 document.addEventListener("DOMContentLoaded", async function (e) {
     // muestra producto de carrito
     getJSONData(CART_INFO_URL).then(function (articulo) {
-        var totales = document.getElementById("totales");
         if (articulo.status === "ok") {
             const product = articulo.data
 
@@ -35,7 +34,6 @@ document.addEventListener("DOMContentLoaded", async function (e) {
                         document.getElementById("cartCount").innerHTML = `<p>Cantidad: ` + cantidad + `</p>`;
                         document.getElementById("subtotal").innerHTML = `<p>SubTotal: \$${parseInt(articles.unitCost) * parseInt(cantidad)}</p>`;
                         document.getElementById("sub").innerHTML = `<p>SubTotal: \$${parseInt(articles.unitCost) * parseInt(cantidad)}</p>`;
-                        document.getElementById("tot").innerHTML = `<p>Total: \$${parseInt(articles.unitCost) * parseInt(cantidad)}</p>`;
                     })
 
                     document.getElementById("menos").addEventListener("click", () => {
@@ -43,31 +41,97 @@ document.addEventListener("DOMContentLoaded", async function (e) {
                         document.getElementById("cartCount").innerHTML = `<p>Cantidad: ` + cantidad + `</p>`;
                         document.getElementById("subtotal").innerHTML = `<p>SubTotal: $` + (parseInt(articles.unitCost) * parseInt(cantidad)) + `</p>`;
                         document.getElementById("sub").innerHTML = `<p>SubTotal: $` + (parseInt(articles.unitCost) * parseInt(cantidad)) + `</p>`;
-                        document.getElementById("tot").innerHTML = `<p>Total: \$${parseInt(articles.unitCost) * parseInt(cantidad)}</p>`;
                     });
 
                     return (parseInt(articles.unitCost) * parseInt(cantidad));
 
                 };
+
+                //Funcion del formulario envio
+                document.getElementById("formEnvioYPago").addEventListener("submit", (e) => {
+                    e.preventDefault()
+
+                    let data = new FormData(e.target);
+                    let tipoEnvio = data.get("tipoEnvio");
+                    let name = document.getElementById("name").value;
+                    let lastname = document.getElementById("lastname").value;
+                    let street = document.getElementById("calle").value;
+                    let nDoor = document.getElementById("numero").value;
+                    let esquina = document.getElementById("esquina").value;
+                    let postal = document.getElementById("postal").value;
+                    let provincia = document.getElementById("provincia").value;
+                    let country = document.getElementById("pais").value;
+
+                    if (name && lastname && street && nDoor && esquina && postal && provincia && country && tipoEnvio) {
+                        var obj = ({
+                            name,
+                            lastname,
+                            street,
+                            nDoor,
+                            esquina,
+                            postal,
+                            provincia,
+                            country,
+                            tipoEnvio,
+                        })
+
+                        sessionStorage.setItem("infoEnvio", JSON.stringify(obj));
+                    } else {
+                        alert("¡¡Rellene campos vacios!!");
+                    }
+
+
+
+                    document.getElementById("costoEnvio").innerHTML = `<p>Costo de Envio: $` + (subTotal() * parseFloat(tipoEnvio)) + `</p>`;
+                    document.getElementById("tot").innerHTML = `<p>Total: $` + ((subTotal() * parseFloat(tipoEnvio)) + subTotal()) + `</p>`;
+
+
+                });
             }
 
         }
+
+
     });
 
+    //form modal
+    document.getElementById("btnGuardar").addEventListener("click", () => {
+        const formaPago = document.getElementById("formaPago").value;
+        const nCuenta = document.getElementById("numeroCuenta").value;
+        const cvv = document.getElementById("codigoSeg").value;
+        const ven = document.getElementById("vencimiento").value;
+        const titular = document.getElementById("titular").value;
+        const direccion = document.getElementById("direccionPago").value;
+        const ciudad = document.getElementById("ciudadPago").value;
+        const postal = document.getElementById("postalPago").value;
 
-    //Funcion del formulario
-    document.getElementById("formEnvioYPago").addEventListener("submit", (e) => {
-        e.preventDefault()
+        if (nCuenta && cvv && ven && titular && direccion && ciudad && postal) {
 
-        let data = new FormData(e.target);
-        let tipoEnvio = data.get("tipoEnvio");
+            var objeto = ({
+                formaPago,
+                nCuenta,
+                cvv,
+                ven,
+                titular,
+                direccion,
+                ciudad,
+                postal,
+            })
 
+            sessionStorage.setItem("infoPago", JSON.stringify(objeto));
+            
+        }else { 
+            alert("¡¡Rellene campos vacios!!");
+        }
+    });
 
-        const formulario = document.getElementById("formEnvioYPago");
-
-        if (formulario) {
-            alert("Compra realizada con exitos!!!");
-
+    // Boton comprar
+    document.getElementById("comprar").addEventListener("click", () => {
+        if (JSON.parse(sessionStorage.getItem("infoPago")) && JSON.parse(sessionStorage.getItem("infoEnvio"))) {
+            alert("¡¡Compra realizada con éxitos!!");
+            window.location.href = "cart.html"
+        } else {
+            alert("¡¡Rellene todos los campos vacios!!")
         }
 
     });
